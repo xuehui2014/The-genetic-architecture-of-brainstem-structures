2e57a8b59572c1a3ea7acd320d2b3bef7d083d0d
xuehui2014 <118502598+xuehui2014@users.noreply.github.com>
2025-05-16 10:43:19 +0800

    Update 3_main.sh

--- 3_main.sh
+++ 3_main.sh
@@ -52,6 +52,29 @@ metal meta_cross_ancestry.txt
 
 metal meta_admixed_ancestry.txt
 
+# Step 6: Pooling-clumping
+
+plink --bfile ${genetic_file}   --clump ${gwas_file}             --clump-p1 5.56e-9            --clump-p2 5e-8       --clump-r2 0.1         --clump-kb 500         --clump-field "P"   --out p_5e-8_9_r2_0.1_window_500_${gwas_file}
+
+# Step 6: Cross-ancestry LD score
+
+plink --bfile ${cross_ancestry_genetic_file} --exclude high-LD-regions.txt --range --out ${cross_ancestry_genetic_file_rm_high-LD}
+plink --bfile ${cross_ancestry_genetic_file_rm_high-LD} --indep-pairwise 50 5 0.2 --out file
+plink --bfile ${cross_ancestry_genetic_file_rm_high-LD} --extract file.prune.in --pca 40 --out cross_ancestry_pca
+python cov-ldsc/ldsc.py  --bfile   ${cross_ancestry_genetic_file} --cov cross_ancestry_pca.eigenvec --l2   --ld-wind-cm 20  --out ${cross_ancestry_genetic_file_ld}
+
+
+# Step 7: h2 calculation
+
+munge_sumstats.py --sumstats ${gwas_file} --out ${gwas_file} --merge-alleles w_hm3.snplist --chunksize 500000
+ldsc.py --h2 ${gwas_file}.sumstats.gz --ref-ld-chr ${ld_file} --w-ld-chr ${ld_file} --out  ${gwas_file}
+
+# Step 8: Cross-ancestry genetic correlation
+
+popcorn fit -v 1 --cfile ${scores_file} --gen_effect --sfile1 ${eas_gwas_file} --sfile2 ${eur_gwas_file} "output/${eas_gwas_file}_${eur_gwas_file}_eff"
+
+popcorn fit -v 1 --cfile ${scores_file}  --gen_effect --sfile1 ${eas_gwas_file} --sfile2 ${eur_gwas_file} "output/${eas_gwas_file}_${eur_gwas_file}_imp"
+
 
 
 
